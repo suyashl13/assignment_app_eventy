13a66bec5439a705f2e1ef230ae55887bfedc3e9
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2023-03-28 16:07:58 +0530

    dev: added orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:assignment_app_eventy/screens/home_page.dart';
 import 'package:assignment_app_eventy/screens/no_internet_page.dart';
 import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Assignment Application',
